a85d3c3e82e2b0a1601497bea854df60cf953f80
pip111194 <pranshurathaour73@gmail.com>
2025-11-20 12:31:43 +0530

    Update helpers init to import all modules properly

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,1 +1,39 @@
-# Helpers module
+"""
+Helpers Module
+Imports all helper modules for music platforms and utilities
+"""
+
+# Import all helper modules
+from . import spotify
+from . import apple_music
+from . import soundcloud
+from . import deezer
+from . import ytdl
+from . import jiosaavn
+from . import gaana
+from . import wynk
+from . import jamendo
+from . import audiomack
+from . import music_platforms
+from . import decorators
+from . import filters
+from . import database
+from . import call_manager
+
+__all__ = [
+    'spotify',
+    'apple_music', 
+    'soundcloud',
+    'deezer',
+    'ytdl',
+    'jiosaavn',
+    'gaana',
+    'wynk',
+    'jamendo',
+    'audiomack',
+    'music_platforms',
+    'decorators',
+    'filters',
+    'database',
+    'call_manager'
+]
