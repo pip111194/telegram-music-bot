e348ebf73bf5b35c5d344fb7fb50bd71b5e2fa4e
pip111194 <pranshurathaour73@gmail.com>
2025-11-20 12:31:35 +0530

    Update handlers init to import all modules properly

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,13 @@
-# Handlers module
+"""
+Handlers Module
+Imports all command handlers for the music bot
+"""
+
+# Import all handler modules
+from . import music
+from . import admin
+from . import misc
+from . import platforms
+from . import free_platforms
+
+__all__ = ['music', 'admin', 'misc', 'platforms', 'free_platforms']
